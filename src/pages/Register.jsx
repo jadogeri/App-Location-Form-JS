ef73331cc823e9b01df53f1bedb694b9fdb1af21
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-08-20 20:40:42 -0500

    updated pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Footer from '../layouts/Footer';
+import { Link } from 'react-router-dom';
+import NoPage from './NoPage';
+// import './App.css';
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
+    <div >
+  <h1>App Location Form</h1>
+    <div className="app-location">
+      <h2>Register Account</h2>
+      <div className="line"><span></span></div>
+      <div className="location"><img src="assets/images/location.png" className="img-responsive" alt="" /></div>
+      <form>
+        <input type="text" className="text" value="E-mail address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'E-mail address';}" />
+        <input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}" />
+        <input type="password" value="Confirm Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Confirm Password';}" />
+        <div className="submit"><input type="submit" onclick="myFunction()" value="Create account" /></div>
+        <div className="clear"></div>
+        <div className="new">
+          <h3><Link to={"/"} >Remember password ?</Link></h3>
+          <h4><Link Link to={"/reset"}>Forgot Password ? Reset Account</Link></h4>
+          <div className="clear"></div>
+        </div>
+      </form>
+    </div>
+  {/* <!--start-copyright--> */}
+  <Footer/>
+  {/* <!--//end-copyright--> */}
+    </div>
+  );
 }
 
-export default Register
+export default Register;
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Footer from '../layouts/Footer';
+import { Link } from 'react-router-dom';
+// import './App.css';
 
 const ResetPassword = () => {
   return (
-    <div>ForgotPassword</div>
-  )
+    <div >
+  <h1>App Location Form</h1>
+    <div className="app-location">
+      <h2>Reset Account</h2>
+      <div className="line"><span></span></div>
+      <div className="location"><img src="assets/images/location.png" className="img-responsive" alt="" /></div>
+      <form>
+        <input type="text" className="text" value="E-mail address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'E-mail address';}" />
+        <div className="submit"><input type="submit" onclick="myFunction()" value="Send" /></div>
+        <div className="clear"></div>
+        <div className="new">
+          <h3><Link to={"/forgot"} >Forgot password ?</Link></h3>
+          <h4><Link Link to={"/"}>Logged Out ? Sign In</Link></h4>
+          <div className="clear"></div>
+        </div>
+      </form>
+    </div>
+  {/* <!--start-copyright--> */}
+  <Footer/>
+  {/* <!--//end-copyright--> */}
+    </div>
+  );
 }
 
-export default ResetPassword
+export default ResetPassword;
